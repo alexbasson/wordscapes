65dc658b6802eecdbc9c3ecdf19d01ac1fbfe0e2
Alex Basson <alexbasson@gmail.com>
2020-02-23 08:52:10 -0500

    Add add.swift

--- add.swift
+++ add.swift
@@ -9,23 +9,23 @@ var words: [String] = []
 do {
     let fileContents = try String(contentsOf: fileURL, encoding: .utf8)
     words = fileContents.components(separatedBy: "\n")
+} catch {
 }
-catch {}
 
-print("Enter words to remove from dictionary (enter a blank line when done):")
-var wordsToRemove: [String] = []
+print("Enter words to add dictionary (enter a blank line when done):")
+var wordsToAdd: [String] = []
 while let line = readLine(), line.count > 0 {
-    wordsToRemove.append(line.lowercased())
+    wordsToAdd.append(line.lowercased())
 }
 
-let filteredWords = words.filter({ word in return !wordsToRemove.contains(word) })
+words.append(contentsOf: wordsToAdd)
 
-let joinedString = filteredWords
+let joinedString = words
     .filter({ $0.count > 0 })
     .sorted(by: { $0 < $1 })
     .joined(separator: "\n")
 
 do {
     try "\(joinedString)\n".write(to: fileURL, atomically: false, encoding: .utf8)
+} catch {
 }
-catch {}
